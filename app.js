32952c45d17d2dfdc9a318324c83da7b68983607
Marie092321 <104540165+Marie092321@users.noreply.github.com>
2022-06-10 12:58:59 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function change() {
+    var e=document.querySelector('.container');
+    e.classList.toggle('active');
+
+    var e=document.querySelector('.container1');
+    e.classList.toggle('active');
+}
+
